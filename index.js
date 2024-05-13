83d0dc3f6c18a3d107b2e131d4a93e38fbdde3a0
mansibansal9648 <mansibansal12345@gmail.com>
2024-05-13 21:50:35 +0530

    enable cors

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ import express from "express";
 import authRouter from "./routes/authRoute.js";
 import dbConnection from "./db/db.js";
 import dotenv from "dotenv";
+import cors from 'cors';
 
 const app = express();
 
 dotenv.config();
 dbConnection();
 
+app.use(cors({
+  origin: "*",
+}))
 app.use(express.json());
 app.use("/api", authRouter);
 
